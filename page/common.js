821f6d9e95b2fbdc231e68f3de6d2c170dcbfc30
WeiChun <weichun@csie.tw>
2018-04-02 12:20:24 +0800

    Add online fetcher of bilibili

--- page/common.js
+++ page/common.js
@@ -1,29 +1,73 @@
-﻿var gotFile = function (name, content) {
+﻿window._curVideoInfo = null;
+window.onVideoInfoRec = function(data) {
+  _curVideoInfo = Object.assign(_curVideoInfo, data);
+  showVideoInfo(data);
+};
+
+var showVideoInfo = function(data) {
+  document.querySelector('#video-info').style.display = 'block';
+  var imgTag = document.querySelector('#video-img');
+  fetch(data.pic + '@300w_300h.webp', {
+    referrerPolicy: "no-referrer"
+  }).then(r=>r.blob()).then(b=>imgTag.src = URL.createObjectURL(b));
+  document.querySelector('#video-title').textContent = data.title;
+  document.querySelector('#video-description').textContent = data.description;
+};
+
+var gotFile = function(name, content) {
   var danmaku = parseFile(content);
   var ass = generateASS(setPosition(danmaku), {
     'title': document.title,
     'ori': name,
+    'alpha': 1,
   });
   startDownload('\ufeff' + ass, name.replace(/\.[^.]*$/, '') + '.ass');
 };
-window.addEventListener('load', function () {
-  var upload = document.querySelector('#upload');
-  upload.addEventListener('change', function () {
-    var file = upload.files[0];
-    var name = file.name;
-    var reader = new FileReader();
-    if (file.size > (1 << 24)) error();
-    else reader.addEventListener('load', function () {
-      gotFile(name, reader.result);
+
+window.addEventListener('load', function() {
+  var fetchVideoInfo = function(aid) {
+    aid = aid.replace(/^av/i, "")
+    window._curVideoInfo = { aid };
+    return new Promise((a,b)=>{
+      var script = document.createElement('script');
+      script.src = "https://api.bilibili.com/view?type=jsonp&appkey=8e9fc618fbd41e28&type=jsonp&callback=onVideoInfoRec&id=" + aid + '&_=' + Math.random();
+      script.addEventListener('load', function() {
+        this.parentNode.removeChild(this);
+        a(_curVideoInfo);
+      });
+      script.addEventListener('error', e=>b(e));
+      script.addEventListener('abort', e=>b(e));
+      document.body.appendChild(script);
+      document.querySelector('#video-info').style.display = 'none';
+      document.querySelector('#video-title').href = document.querySelector('#video-img-container').href = 'https://www.bilibili.com/video/av' + aid;
     });
-    reader.readAsText(file);
-    upload.value = '';
+  };
+
+  var sideForm = document.querySelector('#side-buttons');
+  sideForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    var aid = this['aid'].value.replace(/^av/i, "");
+    new Promise(a=>{
+      if (window._curVideoInfo && _curVideoInfo.aid === aid) {
+        return a(_curVideoInfo);
+      } else {
+        return fetchVideoInfo(aid).then(a);
+      }
+    }).then(data=>{
+      return fetch('https://comment.bilibili.com/' + data.cid + '.xml', {
+        referrerPolicy: "no-referrer"
+      }).then(r=>r.text()).then(xml=>{
+        gotFile(_curVideoInfo.title, xml);
+      })
+    });
+
   });
+
+  if (window.URLSearchParams) {
+    var urlAid = new URLSearchParams(location.search).get('aid');
+    if (urlAid) {
+      sideForm['aid'].value = urlAid;
+      fetchVideoInfo(urlAid);
+    }
+  }
 });
-
-
-if (navigator.userAgent.match(/^Mozilla\/5.0 \([^)]+; rv:[\d.]+\) Gecko\/[\d]{8} Firefox\/[\d.]+$/)) {
-  const style = document.createElement('style');
-  style.innerHTML = '.addon { display: block; }';
-  document.documentElement.appendChild(style);
-}
